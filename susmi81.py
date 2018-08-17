267a4b37993a9f41cdbb25651e606ff688091ceb
AllaSusmitha <41768199+AllaSusmitha@users.noreply.github.com>
2018-08-17 11:43:14 +0530

    Create susmi81.py

---
+++ susmi81.py
@@ -0,0 +1,13 @@
+def spe(s):
+	c=0
+	for i in range (0,len(s)):
+		a=s[i]
+		if ((a>='a' and a<='z')or (a>='A'and a<='Z')):
+			pass
+		elif (a>='0'and a<='9') or  (a ==' '):
+			pass
+		else:
+			c=c+1
+	return c
+s=raw_input()
+print spe(s)
